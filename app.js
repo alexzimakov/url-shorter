ea4203b682644ba1eae5168e4292e9ef088b7bc3
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-11 13:50:49 +0300

    Update app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+/** @module app */
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const middlewares = require('./middlewares');
 
 const app = express();
-
-
-app.use('/', async (req, res) => {
-  res.send('url-shorter api server');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(middlewares.paging);
+app.use('/', (req, res) => res.send('url-shorter api server'));
 
 
 module.exports = app;
