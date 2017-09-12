b949dd3931f91867ca50398716a3f569eb0c3ed6
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-12 20:00:28 +0300

    Implement `parseFilterQueryParameter` middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,12 @@
 /** @module middlewares */
 
+const parseWhereQueryParameter = require('./parseWhereQueryParameter');
 const addPaginationObjectToRequest = require('./addPaginationObjectToRequest');
 const addSortObjectToRequest = require('./addSortObjectToRequest');
 
 
 module.exports = {
+  parseWhereQueryParameter,
   addPaginationObjectToRequest,
   addSortObjectToRequest,
 };
