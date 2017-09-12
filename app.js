08544404f53676b67a461495f458caf7ba85fc37
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-12 13:52:01 +0300

    Remove `paging` middleware

--- app.js
+++ app.js
@@ -2,12 +2,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const middlewares = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(middlewares.paging);
 app.use('/', (req, res) => res.send('url-shorter api server'));
 
 
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,8 @@
 /** @module middlewares */
 
-const paging = require('./paging');
 const addPaginationObjectToRequest = require('./addPaginationObjectToRequest');
 
 
 module.exports = {
-  paging,
   addPaginationObjectToRequest,
 };
