b78f5c5a763fa1b9b8dd1e86b8e0fad41615a200
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-15 18:35:53 +0300

    Implement handlers for users api endpoints

---
+++ api/index.js
@@ -0,0 +1,11 @@
+/** @module api */
+
+const express = require('express');
+const users = require('./users');
+
+const router = express.Router();
+
+router.use(users);
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const api = require('./api');
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/v1', api);
 app.use('/', (req, res) => res.send('url-shorter api server'));
 
 
