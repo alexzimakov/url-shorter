029e520fe39c575a4446ec8f9f409ccf46ec6cea
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-18 01:58:43 +0300

    Implement handler for links api endpoints

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@
 
 const express = require('express');
 const users = require('./users');
+const links = require('./links');
 
 const router = express.Router();
 
 router.use(users);
+router.use(links);
 
 
 module.exports = router;
