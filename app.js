ba3f372069dac37254c35feeca07598f8bf3375d
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-19 00:21:59 +0300

    Configure nunjucks

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
 const api = require('./api');
 const services = require('./services');
 const routes = require('./routes');
 
 const app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
