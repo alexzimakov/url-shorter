cb53d4fb9aaa87052b66b526346f23b3c1f57d99
Alexander Zimakov <alexzimakov@icloud.com>
2017-09-28 18:38:53 +0300

    Add Authorization header to allowed headers

--- middlewares/allowCrossDomain.js
+++ middlewares/allowCrossDomain.js
@@ -1,6 +1,6 @@
 function allowCrossDomain(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 
   if (req.method === 'OPTIONS') {
