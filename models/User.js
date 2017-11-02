5ee3611df62148df13b2a3fcd5346d10760d4f49
Alexander Zimakov <alexzimakov@icloud.com>
2017-11-02 17:34:36 +0300

    Remove try catch wrappers and define fields getter

--- models/User.js
+++ models/User.js
@@ -5,125 +5,108 @@ const { hashPassword } = require('../lib/cryptoUtils');
 
 
 /**
- * 
+ * Creates a new User model instance.
+ * If arguments wasn't given, sets properties to default values.
+ * If arguments it is a js object, property values sets from the given object.
+ * _acl property by default has read and write permissions for that user.
  * 
  * @class User
  * @extends {Base}
  */
 class User extends Base {
-  constructor({
-    username,
-    email,
-    password,
-    firstName,
-    lastName,
-    birthDate,
-    gender,
-    isActive = true,
-  }) {
-    super();
-
-    this._acl = {
-      [this._id]: {
-        r: true,
-        w: true,
+  /**
+   * Returns object which contains model fields definitions.
+   * 
+   * @readonly
+   * @static
+   * @memberof User
+   */
+  static get fields() {
+    return {
+      ...super.fields,
+      _acl: {
+        type: Object,
+        default(instance) {
+          return {
+            [instance._id]: {
+              w: true,
+              r: true,
+            },
+          };
+        },
+      },
+      username: {
+        type: String,
+        index: true,
+      },
+      email: String,
+      password: String,
+      firstName: String,
+      lastName: String,
+      birthDate: Date,
+      gender: String,
+      isActive: {
+        type: Boolean,
+        default: true,
       },
     };
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthDate = birthDate;
-    this.gender = gender;
-    this.isActive = isActive;
   }
 
 
   /**
+   * Sets a new password for user which is presented by model instance.
    * 
-   * 
-   * @readonly
-   * @static
-   * @memberof User
-   */
-  static get collection() {
-    return 'users';
-  }
-
-
-  /**
-   * 
-   * 
-   * @readonly
-   * @static
-   * @memberof User
-   */
-  static get indexes() {
-    return ['username'];
-  }
-
-
-  /**
-   * 
-   * 
-   * @param {String} password 
+   * @param {String} password – Raw password.
    * @returns {Promise}
    * @memberof User
    */
   async setPassword(password) {
-    try {
-      const hashedPassword = await hashPassword(password || this.password);
-      Object.assign(this, { password: hashedPassword });
-    } catch (error) {
-      throw error;
-    }
+    const hashedPassword = await hashPassword(password || this.password);
+    Object.assign(this, { password: hashedPassword });
   }
 
 
   /**
-   * 
+   * Saves a user to database.
+   * Calls this.setPassword before saving.
    * 
    * @returns {Promise}
    * @memberof User
+   * @override
    */
   async save() {
-    try {
-      await this.setPassword();
-      await super.save();
-    } catch (error) {
-      throw error;
-    }
+    await this.setPassword();
+    await super.save();
   }
 
 
   /**
-   * 
+   * Updates an user data in database.
    * 
    * @returns {Promise}
    * @memberof User
+   * @override
    */
   async update(update) {
-    try {
-      delete this.password;
-      await super.update(update);
-    } catch (error) {
-      throw error;
-    }
+    const updateWithoutPassword = Object.assign({}, update);
+
+    delete updateWithoutPassword.password;
+    delete this.password;
+    await super.update(updateWithoutPassword);
   }
 
 
   /**
-   * 
+   * Returns an object version of the User model instance.
    * 
    * @returns {Object}
    * @memberof User
+   * @override
    */
-  toJson() {
-    const target = super.toJson();
+  toObject() {
+    const target = super.toObject();
 
     delete target.password;
-
     return target;
   }
 }
