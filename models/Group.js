8a1e2049c83dc3104ba974c29f89d3c0c90fd9c1
Alexander Zimakov <alexzimakov@icloud.com>
2017-11-02 17:35:01 +0300

    Add new methods

--- models/Group.js
+++ models/Group.js
@@ -1,35 +1,67 @@
 /** @module Group */
 
+const { ObjectID } = require('mongodb');
 const Base = require('./Base');
 
 
+/**
+ * Creates a new Group model instance.
+ * If arguments wasn't given, sets properties to default values.
+ * If arguments it is a js object, property values sets from the given object.
+ * 
+ * @class Group
+ * @extends {Base}
+ */
 class Group extends Base {
-  constructor({ name, pluralName, isDefault, users }) {
-    super();
-
-    this.name = name;
-    this.pluralName = pluralName;
-    this.isDefault = isDefault;
-    this.users = users;
+  /**
+   * Returns object which contains model fields definitions.
+   * 
+   * @readonly
+   * @static
+   * @memberof Group
+   */
+  static get fields() {
+    return {
+      ...super.fields,
+      name: String,
+      pluralName: String,
+      isDefault: Boolean,
+      users: {
+        type: Array,
+        index: true,
+      },
+    };
   }
 
 
-  static get collection() {
-    return 'groups';
-  }
-
-
-  static get indexes() {
-    return ['users'];
-  }
-
+  /**
+   * Returns an initial data for inserting to collection.
+   * 
+   * @readonly
+   * @static
+   * @memberof Group
+   */
   static get initialData() {
+    const adminGroupId = new ObjectID();
     return [{
+      _id: adminGroupId,
+      _acl: {
+        [adminGroupId]: {
+          w: true,
+          r: true,
+        },
+      },
       name: 'Администратор',
       pluralName: 'Администраторы',
       isDefault: false,
       users: [],
     }, {
+      _acl: {
+        [adminGroupId]: {
+          w: true,
+          r: true,
+        },
+      },
       name: 'Автор',
       pluralName: 'Авторы',
       isDefault: true,
@@ -37,6 +69,15 @@ class Group extends Base {
     }];
   }
 
+
+  /**
+   * Adds an user to default group. Returns a promise which resolves
+   * if user was added successfully.
+   * 
+   * @static
+   * @param {any} user – An instance of User model.
+   * @memberof Group
+   */
   static async addUserToDefaultGroup(user) {
     const collection = await this.getCollection();
     await collection.findOneAndUpdate({ isDefault: true }, {
@@ -47,6 +88,14 @@ class Group extends Base {
   }
 
 
+  /**
+   * Removes user from default group. Returns a promise which resolves
+   * if user was removed successfully.
+   * 
+   * @static
+   * @param {any} user 
+   * @memberof Group
+   */
   static async removeUserFromDefaultGroup(user) {
     const collection = await this.getCollection();
     await collection.findOneAndUpdate({ isDefault: true }, {
