66bedd56027b21fdb78d3e01ce64be45fd520cca
Alexander Zimakov <alexzimakov@icloud.com>
2017-11-13 17:06:31 +0300

    Update Link model class

--- models/Link.js
+++ models/Link.js
@@ -1,68 +1,74 @@
 /** @module Link */
 
-const Base = require('./Base');
+const { isEqual } = require('lodash');
+const AbstractModel = require('./abstract-model');
 const { keyFromDate } = require('../lib/utils');
+const { randomHash } = require('../lib/crypto-utils');
 
 
 /**
  * Creates a new Link model instance.
- * If arguments wasn't given, sets properties to default values.
- * If arguments it is a js object, property values sets from the given object.
- * By default _acl propety has read permissions for all users.
  * 
- * @class Link
- * @extends {Base}
+ * @class
+ * @augments AbstractModel
  */
-class Link extends Base {
-  /**
-   * Returns object which contains model fields definitions.
-   * 
-   * @readonly
-   * @static
-   * @memberof Link
-   */
-  static get fields() {
+class Link extends AbstractModel {
+  static get schema() {
     return {
-      ...super.fields,
-      _acl: {
-        type: Object,
-        default: {
-          '*': {
-            w: false,
-            r: true,
-          },
-        },
+      originalUrl: {
+        type: 'url',
+        required: true,
       },
-      url: String,
-      tags: {
-        type: Array,
-        index: true,
-      },
-      description: String,
       hash: {
-        type: String,
+        type: 'hex',
+        index: true,
+        required: true,
+        default: randomHash(6),
+      },
+      author: {
+        type: 'object',
+        required: true,
+      },
+      tags: {
+        type: 'array',
         index: true,
       },
-      clicks: Object,
+      description: {
+        type: 'string',
+      },
+      clicks: {
+        type: 'object',
+      },
     };
   }
 
 
   /**
+   * Checks if a given user is author of link.
+   *
+   * @param {User} user
+   * @returns {Boolean}
+   */
+  isAuthor(user) {
+    return isEqual(user._id, this.author);
+  }
+
+
+  /**
    * Updates number of clicks for current date.
-   * 
-   * @memberof Link
+   *
+   * @returns {Promise.<void>}
    */
   async updateStatistics() {
     const key = keyFromDate();
 
-    if (this.clicks[key]) {
+    if (key in this.clicks) {
       this.clicks[key] += 1;
     } else {
       this.clicks[key] = 1;
     }
 
-    await this.update();
+    await super.update();
   }
 }
 
