046437920ed01a4856dfd2294147e823b1a876e7
Alexander Zimakov <alexzimakov@icloud.com>
2017-11-13 17:15:59 +0300

    Update users and links routes

--- api/links.js
+++ api/links.js
@@ -1,296 +1,168 @@
 /** @module links */
 
 const express = require('express');
-const _ = require('lodash');
-const { ObjectID } = require('mongodb');
-const { ValidationError, ApiError } = require('../lib/error-classes');
-const { authenticate, authorize, validate, parse } = require('../middlewares');
-const { validationResult } = require('express-validator/check');
-const { getInstance } = require('../databaseAdapter');
-const { randomHash } = require('../lib/cryptoUtils');
-const { randomHash } = require('../lib/crypto-utils');
-const { respondWithError, respondWithSuccess } = require('../lib/response-utils');
+const linkController = require('../controllers/link-controller');
+const { authenticate, authorize, parseQuery } = require('../middlewares');
 
 const router = express.Router();
-const ALLOWED_FIELDS = ['url', 'tags', 'description'];
-const OMITTED_FIELDS = ['clicks', 'author'];
 
 
 /**
- * Handler for HTTP GET method to `/api/v1/links` route.
+ * GET request for getting list of links.
  */
 router.get('/links', [
-  parse.query.filter,
-  parse.query.skipAndLimit,
-  parse.query.sort,
-], async (req, res) => {
+  authenticate,
+  authorize('links.view'),
+  parseQuery('filter', 'skip', 'limit', 'sort'),
+], async (req, res, next) => {
   try {
-    const { filter, skip, limit, sort } = req.query;
-    const fields = _.reduce(
-      OMITTED_FIELDS,
-      (obj, field) => _.assign(obj, { [field]: false }),
-      {},
-    );
-    const db = await getInstance();
-    const col = db.collection('links');
-    const getTotal = col.count(filter);
-    const getLinks = col.find(filter, fields)
-      .skip(skip)
-      .limit(limit)
-      .sort(sort)
-      .toArray();
-    const total = await getTotal;
-    const links = await getLinks;
+    const context = {
+      authUser: req.user,
+      query: req.query,
+    };
 
-    respondWithSuccess(res, { count: total, links });
+    res.json({
+      status: 'success',
+      data: await linkController.getMany(context),
+    });
   } catch (error) {
-    respondWithError(res, error);
+    next(error);
   }
 });
 
 
 /**
- * Handler for HTTP POST method to `/api/v1/links` route.
+ * POST request for creating new link.
  */
 router.post('/links', [
   authenticate,
-  authorize(['staff', 'author']),
-  validate.links.create,
-], async (req, res) => {
+  authorize('links.create'),
+], async (req, res, next) => {
   try {
-    // Validates a request body.
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new ValidationError(errors);
-    }
+    const context = {
+      authUser: req.user,
+      data: req.body,
+    };
 
-    const { user } = req.auth;
-    const link = _.reduce(
-      ALLOWED_FIELDS,
-      (obj, field) => _.assign(obj, { [field]: req.body[field] || null }),
-      {},
-    );
-
-    // Define service fields.
-    link.hash = await randomHash(6);
-    link.author = user._id;
-    link.clicks = {};
-    link.createdAt = new Date();
-    link.updatedAt = new Date();
-
-    const db = await getInstance();
-    const col = db.collection('links');
-    const r = await col.insertOne(link);
-
-    if (!_.eq(r.insertedCount, 1)) {
-      throw ApiError('Произошла ошибка при сохранении ссылки в базу данных.');
-    }
-
-    respondWithSuccess(res, { link }, 201);
+    res.json({
+      status: 'success',
+      data: await linkController.create(context),
+    });
   } catch (error) {
-    respondWithError(res, error);
+    next(error);
   }
 });
 
 
 /**
- * Handler for HTTP PUT method to `/api/v1/links` route.
+ * PUT request for updating multiple links.
  */
 router.put('/links', [
   authenticate,
-  authorize(['staff', 'author']),
-  validate.users.update,
-  parse.query.filter,
-], async (req, res) => {
+  authorize('links.edit'),
+  parseQuery('filter'),
+], async (req, res, next) => {
   try {
-    // Validates a request id parameter and request body.
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new ValidationError(errors);
-    }
+    const context = {
+      authUser: req.user,
+      filter: req.query.filter,
+      update: req.body,
+    };
 
-    const { user } = req.auth;
-    const filter = _.defaults(
-      { author: new ObjectID(user._id) },
-      req.query.filter,
-    );
-    const update = _.reduce(
-      ALLOWED_FIELDS,
-      (obj, field) => (_.has(req.body, field) ? _.assign(obj, { [field]: req.body[field] }) : obj),
-      {},
-    );
-
-    if (_.isEmpty(update)) {
-      respondWithSuccess(res, { updatedCount: 0 });
-    } else {
-      update.updatedAt = new Date();
-
-      const db = await getInstance();
-      const col = db.collection('links');
-      const r = await col.updateMany(filter, { $set: update });
-
-      if (!_.eq(r.result.ok, 1)) {
-        throw new ApiError('Произошла ошибка при обновлении ссылок в базе данных.');
-      }
-
-      respondWithSuccess(res, { updatedCount: r.modifiedCount });
-    }
+    await linkController.updateMany(context);
+    res.json({ status: 'success' });
   } catch (error) {
-    respondWithError(res, error);
+    next(error);
   }
 });
 
 
 /**
- * Handler for HTTP DELETE method to `/api/v1/links` route.
+ * DELETE request for deleting multiple links.
  */
 router.delete('/links', [
   authenticate,
-  authorize(['staff', 'author']),
-  parse.query.filter,
-], async (req, res) => {
+  authorize('links.delete'),
+  parseQuery('filter'),
+], async (req, res, next) => {
   try {
-    const { user } = req.auth;
-    const filter = _.defaults(
-      { author: new ObjectID(user._id) },
-      req.query.filter,
-    );
-    const db = await getInstance();
-    const col = db.collection('links');
-    const r = await col.deleteMany(filter);
+    const context = {
+      authUser: req.user,
+      filter: req.query.filter,
+    };
 
-    if (!_.eq(r.result.ok, 1)) {
-      throw new ApiError('Произошла ошибка при удалении ссылок из базы данных.');
-    }
-
-    respondWithSuccess(res, { deletedCount: r.deletedCount });
+    await linkController.deleteMany(context);
+    res.json({ status: 'success' });
   } catch (error) {
-    respondWithError(res, error.message);
+    next(error);
   }
 });
 
 
 /**
- * Handler for HTTP GET method to `/api/v1/links/:id` to route.
+ * GET request for getting single link by id.
  */
-router.get('/links/:id', validate.general.mongoId, async (req, res) => {
+router.get('/links/:id', [
+  authenticate,
+  authorize('links.view'),
+], async (req, res, next) => {
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new ValidationError(errors);
-    }
+    const context = {
+      authUser: req.user,
+      id: req.params.id,
+    };
 
-    const objectId = new ObjectID(req.params.id);
-    const db = await getInstance();
-    const col = db.collection('links');
-    const doc = await col.findOne({ _id: objectId });
-
-    if (_.isNull(doc)) {
-      throw new ApiError('Ссылка не найдена.', 404);
-    }
-
-    respondWithSuccess(res, { link: _.omit(doc, OMITTED_FIELDS) });
+    res.json({
+      status: 'success',
+      data: await linkController.getOne(context),
+    });
   } catch (error) {
-    respondWithError(res, error);
+    next(error);
   }
 });
 
 
 /**
- * Handler for HTTP PUT method to `/api/v1/links/:id` route.
+ * PUT request for updating single link by id.
  */
 router.put('/links/:id', [
   authenticate,
-  authorize(['staff', 'author']),
-  validate.general.mongoId,
-  validate.links.update,
-], async (req, res) => {
+  authorize('links.edit'),
+], async (req, res, next) => {
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new ValidationError(errors);
-    }
+    const context = {
+      authUser: req.user,
+      id: req.params.id,
+      update: req.body,
+    };
 
-    const update = _.reduce(
-      ALLOWED_FIELDS,
-      (obj, field) => (_.has(req.body, field) ? _.assign(obj, { [field]: req.body[field] }) : obj),
-      {},
-    );
-
-    if (_.isEmpty(update)) {
-      respondWithSuccess(res, { updatedCount: 0 });
-    } else {
-      update.updatedAt = new Date();
-
-      const { user } = req.auth;
-      const objectId = new ObjectID(req.params.id);
-      const db = await getInstance();
-      const col = db.collection('links');
-      const doc = await col.findOne({ _id: objectId });
-
-      if (_.isNull(doc)) {
-        throw new ApiError('Ссылка не найдена.', 404);
-      }
-
-      if (!_.isEqual(doc.author, user._id)) {
-        throw new ApiError('Недостаточно прав.', 403);
-      }
-
-      const r = await col.findOneAndUpdate(
-        { _id: objectId },
-        { $set: update },
-        { returnOriginal: false },
-      );
-
-      if (!_.eq(r.ok, 1)) {
-        throw new ApiError('Произошла ошибка при обновлении ссылки в базе данных.');
-      }
-
-      respondWithSuccess(res, { updatedCount: 1, link: r.value });
-    }
+    res.json({
+      status: 'success',
+      data: await linkController.updateOne(context),
+    });
   } catch (error) {
-    respondWithError(res, error);
+    next(error);
   }
 });
 
 
 /**
- * Handler for HTTP DELETE method to `/api/v1/links/:id` route.
+ * DELETE request for deleting single link by id.
  */
 router.delete('/links/:id', [
   authenticate,
-  authorize(['staff', 'author']),
-  validate.general.mongoId,
-], async (req, res) => {
+  authorize('links.delete'),
+], async (req, res, next) => {
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new ValidationError(errors);
-    }
+    const context = {
+      authUser: req.user,
+      id: req.params.id,
+    };
 
-    const { user } = req.auth;
-    const objectId = new ObjectID(req.params.id);
-    const db = await getInstance();
-    const col = db.collection('links');
-    const doc = await col.findOne({ _id: objectId });
-
-    if (_.isNull(doc)) {
-      throw new ApiError('Ссылка не найдена.', 404);
-    }
-
-    if (!_.isEqual(doc.author, user._id)) {
-      throw new ApiError('Недостаточно прав.', 403);
-    }
-
-    const r = await col.findOneAndDelete({ _id: objectId });
-
-    if (!_.eq(r.ok, 1)) {
-      throw new ApiError('Произошла ошибка при удалении ссылки из базы данных.');
-    }
-
-    respondWithSuccess(res, { deletedCount: 1 });
+    await linkController.deleteOne(context);
+    res.json({ status: 'success' });
   } catch (error) {
-    respondWithError(res, error);
+    next(error);
   }
 });
 
