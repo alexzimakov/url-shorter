5250f4f3b9abe374c37959cf47dbecc5c3f62b86
Alexander Zimakov <alexzimakov@icloud.com>
2017-11-13 17:18:23 +0300

    Update app.js and server.js

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const config = require('getconfig');
 const winston = require('winston');
 const app = require('./app');
 const DatabaseAdapter = require('./lib/database-adapter');
-const Group = require('./models/Group');
-const User = require('./models/User');
-const Link = require('./models/Link');
+const User = require('./models/user');
+const Link = require('./models/link');
 
 (async () => {
   try {
     const databaseAdapter = new DatabaseAdapter();
 
     await databaseAdapter.connect();
-    await databaseAdapter.registerModel(Group);
     await databaseAdapter.registerModel(User);
     await databaseAdapter.registerModel(Link);
     app.listen(config.port, () => {
